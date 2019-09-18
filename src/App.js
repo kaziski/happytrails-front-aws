0a45352229d7e13490db53bf29bd28b45d2347e7
Kazumi Karbowski <kazkazinus@gmail.com>
2019-09-18 15:26:12 -0400

    Add geocode npm and have a function that takes an address and get geo code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import Geocode from "react-geocode";
+
+class App extends Component {
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  state = {
+    address: ''
+  }
+
+  handleOnChange(event) {
+    this.setState({
+      address: event.target.value
+    })
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault()
+    this.geoFunction()
+
+  }
+
+
+  geoFunction = () => {
+    Geocode.setApiKey("AIzaSyDZZLXvVCOMNZvWpXNbTztmGWfOFTWuBt0");
+    Geocode.enableDebug();
+    Geocode.fromAddress(this.state.address).then(
+      response => {
+        const { lat, lng } = response.results[0].geometry.location;
+        console.log(lat, lng);
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>App</p>
+          <form onSubmit={(event) => this.handleOnSubmit(event)}>
+            <input
+              type="text"
+              value={this.state.address}
+              onChange={event => this.handleOnChange(event)} />
+            <input type="submit" value="Submit Address" />
+          </form>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;

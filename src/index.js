bda4de1ac1c0fa85f663ea84fbe943bebff45c2f
Kazumi Karbowski <kazkazinus@gmail.com>
2019-09-19 23:08:20 -0400

    cleaned up some code

--- src/index.js
+++ src/index.js
@@ -3,25 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
-// import { createStore, applyMiddleware, compose } from 'redux'
-
 import thunk from 'redux-thunk'
-// import rootReducer from './reducers/index'
+import usersReducer from './reducers/users'
 
-const users = () => []
 
 const reducer = combineReducers({
-  users
+  users: usersReducer
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
-// const store = createStore(
-//   rootReducer,
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-//   applyMiddleware(thunk)
-// )
 
 ReactDOM.render(
   <Provider store={store}>

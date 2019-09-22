31318deb8f0b37f87928f0c59d10d6dbcf34636d
Kazumi Karbowski <kazkazinus@gmail.com>
2019-09-22 10:02:11 -0400

    Wrote test for log in

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -2,17 +2,17 @@ import React from "react";
 import { shallow } from "enzyme";
 import { Provider } from "react-redux";
 import configureMockStore from "redux-mock-store";
-import App from "../App";
+import Login from "../components/users/Login"
 
 const mockStore = configureMockStore();
-const store = mockStore({});
+const store = mockStore({})
 
-describe("App Component", () => {
+describe("Login Component", () => {
   it("should render without throwing an error", () => {
     expect(
       shallow(
         <Provider store={store}>
-          <App />
+          <Login />
         </Provider>
       )
     )

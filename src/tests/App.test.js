3ab8e9406812186094899037f940c65c55c0678c
Kazumi Karbowski <kazkazinus@gmail.com>
2019-09-22 09:35:31 -0400

    First Enzyme test passed CI=true npm test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,56 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { expect } from 'chai'
-import configureMockStore from 'redux-mock-store'
-import Enzyme, { shallow, mount } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-// import App from './App'
-import ConnectedApp, { App } from '../App'
-import Login from '../components/users/Login'
-// import { startLoader, stopLoader } from '../actionCreators'
-// import { setCurrentUser } from '../src/actions/currentUser'
-Enzyme.configure({ adapter: new Adapter() })
-const mockStore = configureMockStore()
-const mockState = {}
+// import React from 'react'
+// import { mount } from 'enzyme'
+// import App from '../App'
+// describe('First React component test with Enzyme', () => {
+//   it('renders without crashing', () => {
+//     mount(<App />);
+//   });
+// });
 
-describe("<App />", function () {
-  describe("<App />'s Dispatching Props:", function () {
+import React from "react";
+import { shallow } from "enzyme";
+import { Provider } from "react-redux";
+import configureMockStore from "redux-mock-store";
+import App from "../App";
 
-    let store = mockStore(mockState)
-    let wrapper
+const mockStore = configureMockStore();
+const store = mockStore({});
 
-    beforeEach(function () {
-      wrapper = mount(
+describe("App Component", () => {
+  it("should render without throwing an error", () => {
+    expect(
+      shallow(
         <Provider store={store}>
-          <ConnectedApp />
-        </ Provider>
+          <App />
+        </Provider>
       )
-    })
-
-    afterEach(function () {
-      store.clearActions()
-    })
-    it('renders without crashing', () => {
-      const div = document.createElement('div');
-      ReactDOM.render(<App />, div);
-      ReactDOM.unmountComponentAtNode(div);
-    });
-    // describe('<Login />', () => {
-    //   it('renders without crashing', () => {
-    //     const wrapper = shallow(<App />)
-    //     expect(wrapper.length).toEqual(1);
-    //   });
-    // });
-
-    // describe('<Login />', () => {
-    //   it('has an text input field', () => {
-    //     const wrapper = shallow(<Login />).dive()
-    //     // expect(wrapper.find('input').length).toEqual(3)
-    //     expect(wrapper.find('input').first().type()).to.equal('input');
-    //   })
-    // })
+    )
   })
 })
 
-
+// it('contains <ChildComponent/> Component', () => {
+//   const wrapper = mount(
+//     <CustomProvider>
+//       <SampleComponent {...defaultProps} />
+//     </CustomProvider>
+//   );
+//   expect(wrapper.find(ChildComponent)).to.have.length(1);
+// });

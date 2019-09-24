9cadd9ec74068656696ddafc37fd387a8eaf9d36
Kazumi Karbowski <kazkazinus@gmail.com>
2019-09-24 15:37:58 -0400

    refactored and created a separate file for store

--- src/index.js
+++ src/index.js
@@ -1,35 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
-import thunk from 'redux-thunk'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import store from './store.js'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import 'bulma-helpers/css/bulma-helpers.min.css'
-// import ''
-// import '../node_fortawesome/react-fontawesome'
 
-import users from './reducers/users'
-import currentUser from './reducers/currentUser'
-import loginForm from './reducers/loginForm'
-
-
-const reducer = combineReducers({
-  //I can do users:users or do like line 13
-  users,
-  currentUser,
-  loginForm
-})
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>, document.getElementById('root'));
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
---
+++ src/store.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import thunk from 'redux-thunk'
+import users from './reducers/users'
+import currentUser from './reducers/currentUser'
+import loginForm from './reducers/loginForm'
+
+
+const reducer = combineReducers({
+  //I can do users:users or do like line 13
+  users,
+  currentUser,
+  loginForm
+})
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+export default store

6a1798b8236c01ad763391e6f90973a0dbb2fb4f
Kazumi Karbowski <kazkazinus@gmail.com>
2019-10-11 17:38:10 -0400

    Styled MyReviews - not sure yet

--- src/Components/reviews/Review.js
+++ src/Components/reviews/Review.js
@@ -1,24 +1,18 @@
 import React from 'react'
 
 const Review = ({ review }) => {
-  //* This is where to format to show all the reviews currentUser has
-
-  console.log("review in Review", review)
   return (
+    <>
+      <article className="message is-link">
+        <div className="message-header">
+          <p><a href={review.api_trail_url} target="_blank" rel="noopener noreferrer">{review.api_trail_name}</a></p>
+        </div>
+        <div class="message-body">
+          {review.comment}
+        </div>
+      </article>
+    </>
 
-    <article class="message is-primary">
-      <div class="message-header">
-        <p><a href={review.api_trail_url} target="_blank" rel="noopener noreferrer">{review.api_trail_name}</a></p>
-      </div>
-      <div class="message-body">
-        {review.comment}
-
-      </div>
-    </article>
-
-    // <div className="has-text-white">
-    //   <div>{review.api_trail_name}, {review.api_trail_name}, {review.api_trail_url}</div>
-    // </div>
   );
 }
 
